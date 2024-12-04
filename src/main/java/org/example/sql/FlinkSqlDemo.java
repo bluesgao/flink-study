d6692c14aefd2f0ea9978dcc99157476d58a8e37
blues <blues@example.com>
2024-12-04 19:19:10 +0800

    update

--- src/main/java/org/example/sql/FlinkSqlDemo.java
+++ src/main/java/org/example/sql/FlinkSqlDemo.java
@@ -2,8 +2,9 @@ package org.example.sql;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.nio.file.Paths;
 
 public class FlinkSqlDemo {
     public static void main(String[] args) throws Exception {
@@ -17,6 +18,12 @@ public class FlinkSqlDemo {
                 .build();
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
 
+        // 相对路径
+        String csvFilePath = "data/input.csv";
+
+        // /Users/gocode/JavaProjects/flink-study/data/input.csv
+        // 转换为绝对路径
+        String absoluteCsvFilePath = Paths.get(csvFilePath).toAbsolutePath().toString();
         // 定义输入数据源
         String createSourceTableDdl = "CREATE TABLE csv_source (" +
                 " user_id INT," +
@@ -24,17 +31,18 @@ public class FlinkSqlDemo {
                 " order_amount DOUBLE" +
                 ") WITH (" +
                 " 'connector' = 'filesystem'," +
-                " 'path' = 'file:///Users/gocode/JavaProjects/flink-study/data/input.csv'," +
+                " 'path' = '/Users/gocode/JavaProjects/flink-study/data/input.csv'," +
                 " 'format' = 'csv'" +
                 ")";
         tableEnv.executeSql(createSourceTableDdl);
 
-        // 编写 SQL 查询
-        String query = "SELECT user_id, SUM(order_amount) AS total_amount FROM csv_source GROUP BY user_id";
-        // 执行查询并打印
-        System.out.println(query);
-        Table resultTable = tableEnv.sqlQuery(query);
-        resultTable.execute().print();
+//        // 编写 SQL 查询
+//        String query = "SELECT user_id, SUM(order_amount) AS total_amount FROM csv_source GROUP BY user_id";
+//        // 执行查询并打印
+//        System.out.println(query);
+//        Table resultTable = tableEnv.sqlQuery(query);
+//        resultTable.execute().print();
+//        tableEnv.executeSql(query).print();
 
         // 如果上面有execute，则以下代码不用了，如果是有界流，以下代码还会报错
 //        try {
@@ -43,23 +51,23 @@ public class FlinkSqlDemo {
 //            throw new RuntimeException(e);
 //        }
 
-//        // 定义输出数据源
-//        String createSinkTableDdl = "CREATE TABLE csv_sink (" +
-//                " user_id INT," +
-//                " total_amount DOUBLE" +
-//                ") WITH (" +
-//                " 'connector' = 'filesystem'," +
-//                " 'path' = 'file:///Users/yclxiao/Project/bigdata/flink-blog/doc/output.csv'," +
-//                " 'format' = 'csv'" +
-//                ")";
-//        tableEnv.executeSql(createSinkTableDdl);
-//
-//        // 执行查询并将结果输出到csv_sink
-//        String query = "INSERT INTO csv_sink " +
-//                "SELECT user_id, SUM(order_amount) as total_amount " +
-//                "FROM csv_source " +
-//                "GROUP BY user_id";
-//        tableEnv.executeSql(query);
+        // 定义输出数据源
+        String createSinkTableDdl = "CREATE TABLE csv_sink (" +
+                " user_id INT," +
+                " total_amount DOUBLE" +
+                ") WITH (" +
+                " 'connector' = 'filesystem'," +
+                " 'path' = '/Users/gocode/JavaProjects/flink-study/data/output'," +
+                " 'format' = 'csv'" +
+                ")";
+        tableEnv.executeSql(createSinkTableDdl);
+
+        // 执行查询并将结果输出到csv_sink
+        String query = "INSERT INTO csv_sink " +
+                "SELECT user_id, SUM(order_amount) as total_amount " +
+                "FROM csv_source " +
+                "GROUP BY user_id";
+        tableEnv.executeSql(query);
 
 //        env.execute("Flink SQL Job");
     }
